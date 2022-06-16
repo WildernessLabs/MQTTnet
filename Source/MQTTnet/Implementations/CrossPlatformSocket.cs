16871f646b2632c5ca62b568df882fe7c4abf651
Alexis Christoforides <alexis@thenull.net>
2022-06-16 15:14:26 -0400

    Always use ipv4

--- Source/MQTTnet/Implementations/CrossPlatformSocket.cs
+++ Source/MQTTnet/Implementations/CrossPlatformSocket.cs
@@ -29,7 +29,7 @@ namespace MQTTnet.Implementations
         {
             // Having this constructor is important because avoiding the address family as parameter
             // will make use of dual mode in the .net framework.
-            _socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             _socketDisposeAction = _socket.Dispose;
         }
